8ffc646883585b381575c78a2e7cdc3c0b88205c
Filipe de  paula <filipedepaula.ads@gmail.com>
2020-08-05 23:47:07 -0300

    Criação do módulo calculadora + ajustes nas rotas

---
+++ src/app/calculadora/calculadora.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CalculadoraComponent } from './';
+
+
+
+@NgModule({
+  declarations: 
+  [
+    CalculadoraComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class CalculadoraModule { }
